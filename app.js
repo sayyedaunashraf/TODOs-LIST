e570d1fdab0f2ef5db6fda3bf40f1a5c2f97a16f
sayyedaunashraf <ashrafsayyedmohdaun@gmail.com>
2026-02-03 03:57:52 +0530

    commit4

---
+++ app.js
@@ -0,0 +1,61 @@
+add = document.getElementById("add");
+function getAndUpdate() {
+    console.log("Updating List..");
+    tit = document.getElementById('title').value;
+    desc = document.getElementById('description').value;
+    if (localStorage.getItem('itemJson') == null) {
+        itemJsonArray = [];
+        itemJsonArray.push([tit, desc]);
+        localStorage.setItem('itemJson', JSON.stringify(itemJsonArray))
+    }
+    else {
+        itemJsonArrayStr = localStorage.getItem('itemJson');
+        itemJsonArray = JSON.parse(itemJsonArrayStr);
+        itemJsonArray.push([tit, desc]);
+        localStorage.setItem('itemJson', JSON.stringify(itemJsonArray))
+    }
+    update()
+}
+function update() {
+    if (localStorage.getItem('itemJson') == null) {
+        itemJsonArray = [];
+        localStorage.setItem('itemJson', JSON.stringify(itemJsonArray))
+    }
+    else {
+        itemJsonArrayStr = localStorage.getItem('itemJson');
+        itemJsonArray = JSON.parse(itemJsonArrayStr);
+    }
+
+    //Populate the table
+
+    tableBody = document.getElementById('tableBody');
+    let str = "";
+    itemJsonArray.forEach((element, index) => {
+        str += `
+        <tr>
+                        <th scope="row">${index + 1}</th>
+                        <td>${element[0]} </td>
+                        <td>${element[1]}</td>
+                        <td><button class="btn btn-sm btn-primary" onclick="deleted(${index})">Delete</button></td>
+                    </tr>`;
+    });
+    tableBody.innerHTML = str;
+}
+add.addEventListener("click", getAndUpdate);
+update();
+function deleted(itemIndex) {
+    console.log("Delete", itemIndex);
+    itemJsonArrayStr = localStorage.getItem('itemJson');
+    itemJsonArray = JSON.parse(itemJsonArrayStr);
+    // delete item
+    itemJsonArray.splice(itemIndex, 1);
+    localStorage.setItem('itemJson', JSON.stringify(itemJsonArray));
+    update();
+}
+function clearStorage() {
+    if (confirm("Do you really want to clear")) {
+        console.log("Clearing the storage....")
+        localStorage.clear();
+        update();
+    }
+}
